45e564e07a80b09824f89d93aa23d3d3914e4b53
micahkepe <micahkepe@gmail.com>
2024-08-02 15:39:49 -0700

    Update formula with correct URL and SHA256

--- Formula/vimtutor-sequel.rb
+++ Formula/vimtutor-sequel.rb
@@ -1,8 +1,8 @@
 class VimtutorSequel < Formula
   desc "Vimtutor Sequel - Advanced Vim Tutor Lessons"
   homepage "https://github.com/micahkepe/vimtutor-sequel"
-  url "https://github.com/user-attachments/files/16475842/vimtutor-sequel-1.0.tar.gz"
-  sha256 "7ee6d69858f508f8db738e8172877523481c91735b8a86b4b64e7a50f0a250b7"
+  url "https://github.com/micahkepe/vimtutor-sequel/releases/download/v1.0/vimtutor-sequel-1.0.tar.gz"
+  sha256 "cff4fc1551dda6cd1be9d530e8ed9e39b22580d406c0e61933af554c67eb7d55"
 
   def install
     bin.install "vimtutor-sequel.sh" => "vimtutor-sequel"

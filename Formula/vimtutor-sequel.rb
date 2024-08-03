46985e8875846124a5dc6558df909991eb0db954
micahkepe <micahkepe@gmail.com>
2024-08-02 21:33:24 -0700

    Update URL and sha for v1.2.2

--- Formula/vimtutor-sequel.rb
+++ Formula/vimtutor-sequel.rb
@@ -1,8 +1,8 @@
 class VimtutorSequel < Formula
   desc "Vimtutor Sequel - Advanced Vim Tutor Lessons"
   homepage "https://github.com/micahkepe/vimtutor-sequel"
-  url "https://github.com/micahkepe/vimtutor-sequel/releases/download/v1.2.1/vimtutor-sequel-1.2.1.tar.gz"
-  sha256 "5d9e628d5220ba037e727f640c627c158a8086279bdef122eaf3cd9cae409708"
+  url "https://github.com/micahkepe/vimtutor-sequel/releases/download/v1.2.2/vimtutor-sequel-1.2.2.tar.gz"
+  sha256 "734971af622870ba629439ce0e19020df3480dff42d3e4750ac361db930cead1"
 
   def install
     bin.install "vimtutor-sequel.sh" => "vimtutor-sequel"

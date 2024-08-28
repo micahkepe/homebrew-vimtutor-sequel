25c02a6df6740de5846779981699800f817ea271
micahkepe <micahkepe@gmail.com>
2024-08-27 19:20:32 -0500

    Update brew formula based on audit

--- Formula/vimtutor-sequel.rb
+++ Formula/vimtutor-sequel.rb
@@ -1,9 +1,9 @@
 class VimtutorSequel < Formula
-  desc "Vimtutor Sequel - Advanced Vim Tutor Lessons"
+  desc "Advanced vimtutor for intermediate vim users"
   homepage "https://github.com/micahkepe/vimtutor-sequel"
   url "https://github.com/micahkepe/vimtutor-sequel/releases/download/v1.3.1/vimtutor-sequel-1.3.1.tar.gz"
   sha256 "190627358111d73170d4b1bc7a9823c511b44a71068a8c54207fdd116f4c2152"
-  version "1.3.1"
+  license "MIT"
 
   def install
     bin.install "vimtutor-sequel.sh" => "vimtutor-sequel"
@@ -19,6 +19,6 @@ class VimtutorSequel < Formula
 
   test do
     assert_predicate testpath/"#{pkgshare}/vimtutor-sequel.txt", :exist?
-    system "#{bin}/vimtutor-sequel", "--version" # vimtutor-sequel should return its version
+    system bin/"vimtutor-sequel", "--version" # vimtutor-sequel should return its version
   end
 end
